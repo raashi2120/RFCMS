bdec58362e47500590c6ae85cbe122cfc069a788
raashi2120 <sharmaraashi21@gmail.com>
2026-03-23 16:51:49 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/example/RFCMS/RfcmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.RFCMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RfcmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RfcmsApplication.class, args);
+	}
+
+}
