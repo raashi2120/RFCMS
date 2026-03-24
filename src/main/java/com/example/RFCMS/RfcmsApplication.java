a967a2cb38bdfeffd1c485b89fdef5c29b7eb57a
Manya Goel <manyagoel2014@gmail.com>
2026-03-24 21:10:51 +0530

    Add files via upload

--- src/main/java/com/example/RFCMS/RfcmsApplication.java
+++ src/main/java/com/example/RFCMS/RfcmsApplication.java
@@ -1,13 +1,34 @@
 package com.example.RFCMS;
 
+import com.example.RFCMS.service.RailwayDijkstra;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
-public class RfcmsApplication {
+public class RfcmsApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RfcmsApplication.class, args);
-	}
+    @Autowired
+    private RailwayDijkstra dijkstra;
 
+    public static void main(String[] args) {
+        SpringApplication.run(RfcmsApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter source city: ");
+        String start = sc.nextLine();
+
+        System.out.print("Enter destination city: ");
+        String end = sc.nextLine();
+
+        dijkstra.run(start, end);
+    }
 }
