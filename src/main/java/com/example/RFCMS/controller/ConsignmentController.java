52d70243d1b6940638ab21d6fceab38d30f7446d
Manya Goel <manyagoel2014@gmail.com>
2026-03-31 19:57:19 +0530

    Add files via upload

--- src/main/java/com/example/RFCMS/controller/ConsignmentController.java
+++ src/main/java/com/example/RFCMS/controller/ConsignmentController.java
@@ -2,28 +2,24 @@ package com.example.RFCMS.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import com.example.RFCMS.models.Consignment;
-import com.example.RFCMS.repository.ConsignmentRepository;
 import com.example.RFCMS.service.ConsignmentService;
 
-
 @RestController
-@RequestMapping("/api/consignments")
+@RequestMapping("/consignment")
 public class ConsignmentController {
-
-    @Autowired
-    private ConsignmentRepository consignmentRepository;
 
     @Autowired
     private ConsignmentService consignmentService;
 
-    @PostMapping("/add")
-    public Consignment addConsignment(@RequestBody Consignment c) {
-        return consignmentRepository.save(c);
+    @PostMapping("/book")
+    public Consignment bookConsignment(@RequestBody Consignment c) {
+        return consignmentService.bookConsignment(c);
     }
 
-    @PostMapping("/book")
-    public Consignment book(@RequestBody Consignment c) {   
-        return consignmentService.bookConsignment(c);
+    @PostMapping("/pickup/{id}")
+    public String generateInvoice(@PathVariable String id) {
+        return consignmentService.generateInvoiceOnPickup(id);
     }
 }
