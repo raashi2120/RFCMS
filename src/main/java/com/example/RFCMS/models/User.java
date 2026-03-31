6de3a3642ccfd4a3370e29b3e3f7779ba175a6d0
Manya Goel <manyagoel2014@gmail.com>
2026-03-31 18:47:49 +0530

    Add files via upload

--- src/main/java/com/example/RFCMS/models/User.java
+++ src/main/java/com/example/RFCMS/models/User.java
@@ -1,20 +1,34 @@
 package com.example.RFCMS.models;
 
-import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Data
-@Document(collection = "users")
+@Document(collection = "login")
 public class User {
 
     @Id
-    private String id;
-
-    private String name;
-    private String email;
+    private String userId;
+    private String username;
     private String password;
+    private String role; // USER, ADMIN, STATION_MANAGER
 
-    // VERY IMPORTANT
-    private String role; // USER / ADMIN / STATION_MANAGER
+    public User() {}
+
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    // Getters & Setters
+    public String getUserId() { return userId; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
 }
